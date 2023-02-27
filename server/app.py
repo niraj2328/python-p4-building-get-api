b3f7757f62a0b8d18c962176fef60231b3c0800f
Niraj <niraj.patel@flatironschool.com>
2023-02-27 06:35:47 -0500

    completed

--- server/app.py
+++ server/app.py
@@ -1,25 +1,44 @@
-#!/usr/bin/env python3
-
-#!/usr/bin/env python3
+# server/app.py
 
 from flask import Flask, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-from models import db, User, Review, Game
-
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
 
+db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-db.init_app(app)
+class Game(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    genre = db.Column(db.String)
+    platform = db.Column(db.String)
+    price = db.Column(db.Float)
+
+    def to_dict(self):
+        return {
+            'title': self.title,
+            'genre': self.genre,
+            'platform': self.platform,
+            'price': self.price,
+        }
 
 @app.route('/')
 def index():
     return "Index for Game/Review/User API"
+
+@app.route('/games')
+def games():
+    games = [game.to_dict() for game in Game.query.all()]
+    return make_response(jsonify(games), 200)
+
+@app.route('/games/<int:id>')
+def game_by_id(id):
+    game = Game.query.get_or_404(id)
+    return make_response(jsonify(game.to_dict()), 200)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,8 +1,18 @@
+# server/models.py
+
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_serializer import SerializerMixin
 
 db = SQLAlchemy()
 
-class Game(db.Model):
+
+class BaseModel(db.Model, SerializerMixin):
+    __abstract__ = True
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+
+class Game(BaseModel):
     __tablename__ = 'games'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -10,36 +20,37 @@ class Game(db.Model):
     genre = db.Column(db.String)
     platform = db.Column(db.String)
     price = db.Column(db.Integer)
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    reviews = db.relationship('Review', backref='game')
+    reviews = db.relationship('Review', backref='game', cascade='all, delete-orphan')
+
+    serialize_rules = ('-reviews.game',)
 
     def __repr__(self):
         return f'<Game {self.title} for {self.platform}>'
 
-class Review(db.Model):
+
+class Review(BaseModel):
     __tablename__ = 'reviews'
-    
+
     id = db.Column(db.Integer, primary_key=True)
     score = db.Column(db.Integer)
     comment = db.Column(db.String)
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    game_id = db.Column(db.Integer, db.ForeignKey('games.id'))
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    game_id = db.Column(db.Integer, db.ForeignKey('games.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    serialize_rules = ('-game.reviews', '-user.reviews',)
 
     def __repr__(self):
         return f'<Review ({self.id}) of {self.game}: {self.score}/10>'
 
-class User(db.Model):
+
+class User(BaseModel):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
-    
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    reviews = db.relationship('Review', backref='user')
+    reviews = db.relationship('Review', backref='user', cascade='all, delete-orphan')
+
+    serialize_rules = ('-reviews.user',)
